07ca132972da920281185f5ea28df5cf3a431107
Andre <contact@andres.com>
2026-05-15 15:20:07 -0500

    feat: Actualización de maquetación HTML y CSS

---
+++ src/main/java/funeraleslaluz/com/controlador/GlobalControllerAdvice.java
@@ -0,0 +1,13 @@
+package funeraleslaluz.com.controlador;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    @ModelAttribute("currentUri")
+    public String getCurrentUri(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+}
